1d387da6d40eb23bb4df08489df9c62aecd8739c
shahbaznew <shahbazayub0@gmail.com>
2024-09-23 22:49:18 +0100

    Adding scraped content to the UI

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import (scrape_website, extract_body_content, clean_body_content, split_dom_content,)
 
-st.title("AI Web Scrapper")
+st.title("AI Web Scraper")
 url = st.text_input("Enter a website URL...")
 
 if st.button("Scrape Site"):
-    st.write("Scrape the website")
-    result = scrape_website(url)
-    print(result)
-    
-    
+    if url:
+        st.write("Scraping the website...")
+        result = scrape_website(url)
+        body_content = extract_body_content(result)
+        cleaned_content = clean_body_content(body_content)
+        
+        st.session_state.dom_content = cleaned_content 
+        
+        with st.expander("View DOM Content"): 
+            st.text_area("DOM Content", cleaned_content, height=300)
--- scrape.py
+++ scrape.py
@@ -13,12 +13,11 @@ def scrape_website(website):
         driver.get(website)
         print("Page Loaded...")
         html = driver.page_source 
-    
-    
-        
         return html 
+    finally: 
+        driver.quit()
 def extract_body_content(html_content): 
-    soup = BeautifulSoup(html_content "html.parser")
+    soup = BeautifulSoup(html_content, "html.parser")
     body_content = soup.body 
     if body_content: 
         return str(body_content)
